3c46f076f4b64ceaf0424ae81b4f8c1a51abda5e
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2012-05-26 21:58:58 +0530

    Add help and wrong command options

--- terminal.js
+++ terminal.js
@@ -1,5 +1,38 @@
+var cmd;
+
+function createPrompt() {
+    $('.container').append('<div class="ps"><label for="input" id="prompt"><span id="dir">/</span> $</label><div class="inputbox"><input type="text" id="input"/></div></div><div class="result"></div>');
+    
+}
+
+function convertTextBoxToP() {
+    $('.ps').replaceWith('<p><span id ="dir"> / </span> : '+ cmd +'</p>')
+}
+
+function showHelp() {
+    $('.result:last').append('<div id="hideit" class="help"><p> commands are <br /></div>');
+    $('.help').slideDown('fast');
+    createPrompt();
+}
+
+function wrongCommand() {
+    $('.result:last').append('<div id="hideit" class="wrongCommand"><p> '+cmd+' does not exist. Check `help`.');
+    $('.wrongCommand').slideDown('fast');
+    createPrompt();
+}
 $(document).ready(function() {
-    $('#input').keypress(function(event) {
-        console.log(event.keyCode);
+
+    createPrompt();
+
+    $('#input').live('keypress', function(event) {
+        if( event.keyCode == 13) {
+            cmd = $(this).val();
+            convertTextBoxToP();
+            switch(cmd)
+            {
+                case "help": console.log("help"); showHelp(); break;
+                default : console.log("wrong command"); wrongCommand(); break;
+            }
+        }
     });
 });
