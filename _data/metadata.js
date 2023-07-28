4a48ab95d18e7bc848a31308a7388d2bcc3b161a
Sathya Gunasekaran <gsathya.ceg@gmail.com>
2023-07-28 15:18:54 +0100

    use 11ty

---
+++ _data/metadata.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "Recompiled",
+  url: "https://www.recompiled.dev",
+  language: "en",
+  description: "I write about compilers and JavaScript",
+  author: {
+    name: "Sathya Gunasekran",
+    email: "gsathya.ceg@gmail.com",
+  },
+};
---
+++ content/about/index.md
@@ -0,0 +1,13 @@
+---
+layout: layouts/base.njk
+eleventyNavigation:
+  key: About Me
+  order: 3
+---
+
+# About Me
+
+I work on [React](https://react.dev/) at Meta. Previously, I used to work on
+[V8](https://v8.dev/) at Google.
+
+Chennai → SF → London ⛳
