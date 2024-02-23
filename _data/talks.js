b20cf3aa79b82a21ad700295222e84aadf4bead4
Sathya Gunasekaran <gsathya.ceg@gmail.com>
2024-02-23 17:32:01 +0000

    add more talks

--- _data/talks.js
+++ _data/talks.js
@@ -1,7 +1,26 @@
 module.exports = [
 	{
 		title: "React Forget: React for developers and compilers",
+		event: "React India 2023",
 		url: "https://www.youtube.com/watch?v=kjOacmVsLSE",
 		date: "7 Nov 2023 13:23:12 GMT",
+	},
+	{
+		title: "What's new in JavaScript",
+		event: "Google I/O 2019",
+		url: "https://www.youtube.com/watch?v=c0oy0vQKEZE",
+		date: "31 Jan 2019 13:23:12 GMT",
+	},
+	{
+		title: "V8: Life of a <script>",
+		event: "Chrome University 2018",
+		url: "https://www.youtube.com/watch?v=voDhHPNMEzg",
+		date: "31 Jan 2019 13:23:12 GMT",
+	},
+	{
+		title: "What's new in JavaScript",
+		event: "Google I/O 2018",
+		url: "https://www.youtube.com/watch?v=mIWCLOftfRw",
+		date: "9 May 2018 13:23:12 GMT",
 	},
 ];
